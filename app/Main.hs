646af3708c3357343e5e68783e87856374cd2050
mslxl <i@mslxl.com>
2023-06-08 02:39:58 +0800

    feat: add command option parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,15 @@
-import Application () -- for YesodDispatch instance
+import Application ()
+-- for YesodDispatch instance
 import Foundation
-import Yesod.Core
+import Options.Applicative (execParser, fullDesc, info)
 import Opts
+import Yesod.Core
+
+exec :: CWTask -> IO ()
+exec (Serve port) = putStrLn ("Server starting on port " ++ show port) >> warp port App
+exec _ = undefined
 
 main :: IO ()
-main = warp 3000 App
+main = exec =<< execParser opts
+  where
+    opts = info cwOpts fullDesc
--- app/Opts.hs
+++ app/Opts.hs
@@ -1,8 +1,79 @@
 module Opts where
 
+import Data.Semigroup ((<>))
 import Model
+import Options.Applicative
 
-data Task = 
-  Serve Int
+data CWTask
+  = Serve Int
   | RefreshDB String
   | InsertItem ContestItem
+
+serveCommand :: Parser CWTask
+serveCommand =
+  Serve
+    <$> option
+      auto
+      ( long "port"
+          <> short 'p'
+          <> metavar "PORT"
+          <> value 3000
+          <> help "Server port to be binding"
+      )
+    <**> helper
+
+refreshCommand :: Parser CWTask
+refreshCommand =
+  RefreshDB
+    <$> strOption
+      ( metavar "URL"
+          <> value "https://contests.sdutacm.cn/contests.json"
+          <> help "Recent contest source, see https://github.com/MeiK2333/recent_contests for more infomation"
+      )
+    <**> helper
+
+insertCommand :: Parser CWTask
+insertCommand = fmap InsertItem itemParser
+  where
+    itemParser =
+      ContestItem
+        <$> option
+          auto
+          ( long "start"
+              <> metavar "TIMESTAMP"
+              <> help "Contest start time"
+          )
+        <*> option
+          auto
+          ( long "duration"
+              <> metavar "MILLSECOND"
+              <> value 0
+              <> help "Contest lasted time"
+          )
+        <*> strOption
+          ( long "source"
+              <> short 's'
+              <> help "Contest source"
+          )
+        <*> strOption
+          ( long "name"
+              <> short 'n'
+              <> help "Contest name"
+          )
+        <*> optional
+          ( strOption
+              ( long "link"
+                  <> short 'l'
+                  <> help "Contest link"
+              )
+          )
+        <**> helper
+
+cwOpts :: Parser CWTask
+cwOpts =
+  subparser
+    ( command "serve" (info serveCommand (fullDesc <> progDesc "Start server"))
+        <> command "refresh" (info refreshCommand (fullDesc <> progDesc "Refresh contest info from remote"))
+        <> command "insert" (info insertCommand (fullDesc <> progDesc "Insert local contest info"))
+    )
+    <**> helper
