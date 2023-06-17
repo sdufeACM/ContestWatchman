9e7d28874d91c58732dc5d2ed60d9aa90a4c2f20
mslxl <i@mslxl.com>
2023-06-18 00:44:36 +0800

    feat: support rss subscribe

--- src/model.rs
+++ src/model.rs
@@ -1,21 +1,20 @@
-use serde::{
-    Deserialize, Serialize,
-};
-use time::{OffsetDateTime, Duration};
+use askama::Template;
+use serde::{Deserialize, Serialize};
 use std::{
     collections::hash_map::DefaultHasher,
     hash::{Hash, Hasher},
     ops::Sub,
 };
+use time::{Duration, OffsetDateTime, UtcOffset};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Contest {
     pub source: String,
     pub name: String,
     pub link: Option<String>,
-    #[serde(with="time::serde::iso8601")]
+    #[serde(with = "time::serde::iso8601")]
     pub start_time: OffsetDateTime,
-    #[serde(with="time::serde::iso8601")]
+    #[serde(with = "time::serde::iso8601")]
     pub end_time: OffsetDateTime,
 }
 
@@ -34,7 +33,7 @@ impl Contest {
         if self.start_time > self.end_time {
             Duration::new(0, 0)
         } else {
-          self.end_time.sub(self.start_time)
+            self.end_time.sub(self.start_time)
         }
     }
 
@@ -43,126 +42,58 @@ impl Contest {
         self.hash(&mut hasher);
         hasher.finish()
     }
+
+    pub fn to_offset(mut self, offset: UtcOffset) -> Self{
+        self.start_time = self.start_time.to_offset(offset);
+        self.end_time = self.end_time.to_offset(offset);
+        self
+    }
 }
 
-// impl<'de> Deserialize<'de> for SystemTimeDef {
-//     fn deserialize<D>(deserializer: D) -> Result<SystemTimeDef, D::Error>
-//     where
-//         D: Deserializer<'de>,
-//     {
-//         struct SystemTimeVisitor;
-//         impl<'de> Visitor<'de> for SystemTimeVisitor {
-//             type Value = SystemTimeDef;
-//             fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-//                 formatter.write_str("str")
-//             }
-//             fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-//             where
-//                 E: de::Error,
-//             {
-//                 Ok(SystemTimeDef(v.to_owned()))
-//             }
-//         }
-//         deserializer.deserialize_str(SystemTimeVisitor)
-//     }
-// }
+#[derive(Template)]
+#[template(path="atom.template.xml")]
+pub struct AtomContext {
+    entries: Vec<AtomEntry>,
+    last_build: OffsetDateTime,
+    url: String,
+}
 
-// impl<'de> Deserialize<'de> for Contest {
-//     fn deserialize<D>(deserializer: D) -> Result<Contest, D::Error>
-//     where
-//         D: Deserializer<'de>,
-//     {
-//         enum Field {
-//             Source,
-//             Link,
-//             Name,
-//             StartTime,
-//             EndTime,
-//             Other,
-//         }
+struct AtomEntry {
+    id: u64,
+    duration: f32,
+    content: Contest,
+}
 
-//         impl<'de> Deserialize<'de> for Field {
-//             fn deserialize<D>(deserializer: D) -> Result<Field, D::Error>
-//             where
-//                 D: Deserializer<'de>,
-//             {
-//                 struct FieldVisitor;
+impl From<Contest> for AtomEntry {
+    fn from(v: Contest) -> Self {
+        let duration = v.duration();
+        let minutes = (duration.whole_minutes() % 60) as f32;
+        let hours = duration.whole_hours() as f32;
+        let duration = hours + minutes / 60.0;
+        let duration = (duration * 10.0).round() / 10.0;
 
-//                 impl<'de> Visitor<'de> for FieldVisitor {
-//                     type Value = Field;
+        Self {
+            id: v.id(),
+            duration: duration,
+            content: v,
+        }
+    }
+}
 
-//                     fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-//                         formatter.write_str("source, link, name, start_time or end_time")
-//                     }
-//                     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-//                     where
-//                         E: de::Error,
-//                     {
-//                         match v {
-//                             "source" => Ok(Field::Source),
-//                             "link" => Ok(Field::Link),
-//                             "name" => Ok(Field::Name),
-//                             "start_time" => Ok(Field::StartTime),
-//                             "end_time" => Ok(Field::EndTime),
-//                             _ => Ok(Field::Other),
-//                         }
-//                     }
-//                 }
+impl AtomContext {
+    pub fn new(value: Vec<Contest>, url: String) -> Self {
+        let last_build = value
+            .iter()
+            .map(|v| v.start_time)
+            .min()
+            .unwrap_or_else(|| OffsetDateTime::UNIX_EPOCH);
 
-//                 deserializer.deserialize_identifier(FieldVisitor)
-//             }
-//         }
+        let entries = value.into_iter().map(|v| v.into()).collect();
 
-//         struct ContestVisitor;
-//         impl<'de> Visitor<'de> for ContestVisitor {
-//             type Value = Contest;
-
-//             fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-//                 formatter.write_str("struct Contest")
-//             }
-
-//             fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
-//             where
-//                 A: serde::de::MapAccess<'de>,
-//             {
-//                 let mut source = None;
-//                 let mut name = None;
-//                 let mut link = None;
-//                 let mut start_time = None;
-//                 let mut end_time = None;
-
-//                 while let Some(key) = map.next_key()? {
-//                     match key {
-//                       Field::Name => name = Some(map.next_value()?),
-//                       Field::Link => link = Some(map.next_value()?),
-//                       Field::Source => source = Some(map.next_value()?),
-//                       Field::StartTime => start_time = Some(map.next_value()?),
-//                       Field::EndTime => end_time = Some(map.next_value()?),
-//                       _ => {}
-//                     }
-//                 }
-
-//                 let source = source.ok_or_else(|| de::Error::missing_field("source"))?;
-//                 let name = name.ok_or_else(|| de::Error::missing_field("name"))?;
-//                 let start_time: String =
-//                     start_time.ok_or_else(|| de::Error::missing_field("start_time"))?;
-//                 let end_time: String =
-//                     end_time.ok_or_else(|| de::Error::missing_field("end_time"))?;
-
-//                 let start_time = start_time.parse::<DateTime<Utc>>().unwrap();
-//                 let end_time = end_time.parse::<DateTime<Utc>>().unwrap();
-
-//                 Ok(Contest {
-//                     source,
-//                     name,
-//                     link,
-//                     start_time: start_time.into(),
-//                     end_time: end_time.into(),
-//                 })
-//             }
-//         }
-
-//         const FIELDS: &'static [&'static str] = &["source", "name", "link", "start_time", "end_time"];
-//         deserializer.deserialize_struct("Contest", FIELDS,  ContestVisitor)
-//     }
-// }
+        Self {
+            entries,
+            last_build,
+            url,
+        }
+    }
+}
--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,10 @@
-use std::collections::HashSet;
+use std::{collections::HashSet};
+use askama::Template;
 
-use time::OffsetDateTime;
+use time::{OffsetDateTime, macros::offset};
 use warp::Filter;
 
-use crate::db::{open_db, query_unend_contest};
+use crate::{db::{open_db, query_unend_contest}, model::{Contest, AtomContext}};
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 struct QueryParam {
@@ -11,42 +12,63 @@ struct QueryParam {
     source: Option<String>,
 }
 
-async fn handler(params: QueryParam) -> Result<impl warp::Reply, warp::Rejection> {
-    // Ok(warp::reply::json(&params))
-    dbg!(&params);
-    let db = open_db();
-    let allow = if let Some(all) = params.source {
-      let keys = all.split(",").map(|v| v.trim().to_owned());
-      HashSet::from_iter(keys)
-    } else{
+impl QueryParam{
+  fn apply_filter<T>(&self, iter: T)-> Vec<Contest> where T: Iterator<Item = Contest> {
+    let days = self.day.unwrap_or(i64::MAX);
+    let allow_src = if let Some(ref rules) = self.source {
+      HashSet::from_iter(rules.split(',').map(str::trim))
+    } else {
       HashSet::new()
     };
-    let contest = query_unend_contest(&db)
-        .unwrap()
-        .into_iter()
-        .filter(|item| {
-            if let Some(day) = params.day {
-                (item.start_time - OffsetDateTime::now_utc()).whole_days() <= day
-            } else {
-                true
-            }
-        })
-        .filter(|item| {
-          if allow.is_empty() {
-            true
-          }else{
-            allow.contains(&item.source)
-          }
-        })
-        .collect::<Vec<_>>();
-    Ok(warp::reply::json(&contest))
+
+    let now_utc = OffsetDateTime::now_utc();
+
+    iter
+      .filter(|item| item.start_time.year() >= now_utc.year())
+      .filter(|item| (item.start_time - now_utc).whole_days() < days)
+      .filter(|item| allow_src.is_empty() || allow_src.contains(item.source.as_str()))
+      .map(|item| item.to_offset(offset!(+8)))
+      .collect()
+  }
+}
+
+async fn json_handler(params: QueryParam) -> Result<impl warp::Reply, warp::Rejection> {
+    // Ok(warp::reply::json(&params))
+
+  let db = open_db();
+  let data = query_unend_contest(&db).unwrap();
+  let data = params.apply_filter(data.into_iter());
+  Ok(warp::reply::json(&data))
+}
+
+
+async fn rss_handler(params: QueryParam) -> Result<impl warp::Reply, warp::Rejection>{
+  let db = open_db();
+  let data = query_unend_contest(&db).unwrap();
+  let data = params.apply_filter(data.into_iter());
+
+
+  let atom = AtomContext::new(data, "".to_owned());
+  let content = atom.render();
+  Ok(warp::reply::with_header(content.unwrap(), "Content-Type", "text/xml"))
 }
 
 pub async fn serve(port: u16) {
     let page_db = warp::get()
+        .and(warp::path("data"))
         .and(warp::path::end())
         .and(warp::query::<QueryParam>())
-        .and_then(handler);
+        .and_then(json_handler);
 
-    warp::serve(page_db).run(([0, 0, 0, 0], port)).await;
+      let page_rss = warp::get()
+        .and(warp::path("atom.xml"))
+        .and(warp::path::end())
+        .and(warp::query::<QueryParam>())
+        .and_then(rss_handler);
+
+      let routes = warp::get().and(
+        page_db.or(page_rss)
+      );
+
+    warp::serve(routes).run(([0, 0, 0, 0], port)).await;
 }
